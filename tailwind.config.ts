5342fff9999693385dfb6501fd9c6cf466392066
Anwar Parvez <parvez.uz@gmail.com>
2026-01-18 15:55:44 +0600

    Build error fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class", // ✅ FIXED
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
